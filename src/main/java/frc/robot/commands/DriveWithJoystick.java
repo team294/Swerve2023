4cf0799c9612b507fc981a20b9e72596c1996db8
BCR <tlu21545@gmail.com>
2023-01-15 15:51:51 -0800

    open loop parameters changed

--- src/main/java/frc/robot/commands/DriveWithJoystick.java
+++ src/main/java/frc/robot/commands/DriveWithJoystick.java
@@ -78,7 +78,7 @@ public class DriveWithJoystick extends CommandBase {
     // }
     
     // TODO Calibrate closed-loop control in SwerveModule.setDesiredState, then decide if open vs closed loop should be used here
-    driveTrain.drive(fwdVelocity, leftVelocity, turnRate, true, true);
+    driveTrain.drive(fwdVelocity, leftVelocity, turnRate, true, false);
 
     // lastFwdPercent = fwdPercent;
     // lastTime = curTime;
